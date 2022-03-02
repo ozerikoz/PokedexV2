3b66dc385f134e22f7c70616165439a24a0cc0a7
Erik <erik.paschoalatto1@gmail.com>
2022-03-02 18:52:20 -0300

    feat: add function to get pokemon card color

--- lib/static.dart
+++ lib/static.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Static {
-  //fonts
+  //Fonts
   TextStyle fontStyleBold(double size, Color color) {
     return TextStyle(
       fontFamily: "Poppins",
@@ -36,5 +36,49 @@ class Static {
       color: color,
       fontWeight: FontWeight.w300,
     );
+  }
+
+  //Colors
+  Color? color({required String type}) {
+    switch (type) {
+      case 'Normal':
+        return Colors.brown[400];
+      case 'Fire':
+        return Colors.red;
+      case 'Water':
+        return Colors.blue;
+      case 'Grass':
+        return Colors.green;
+      case 'Electric':
+        return Colors.amber;
+      case 'Ice':
+        return Colors.cyanAccent[400];
+      case 'Fighting':
+        return Colors.orange;
+      case 'Poison':
+        return Colors.purple;
+      case 'Ground':
+        return Colors.orange[300];
+      case 'Flying':
+        return Colors.indigo[200];
+      case 'Psychic':
+        return Colors.pink;
+      case 'Bug':
+        return Colors.lightGreen[500];
+      case 'Rock':
+        return Colors.grey;
+      case 'Ghost':
+        return Colors.indigo[400];
+      case 'Dark':
+        return Colors.brown;
+      case 'Dragon':
+        return Colors.indigo[800];
+      case 'Steel':
+        return Colors.blueGrey;
+      case 'Fairy':
+        return Colors.pinkAccent[100];
+      default:
+        return Colors.grey;
+    }
   }
 }
