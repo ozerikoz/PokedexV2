71d4ccc7debae5916491f35b7c8c5ff83191ceb9
Erik <erik.paschoalatto1@gmail.com>
2022-03-14 01:16:07 -0300

    feat: new pokelist card design implemented, colors switch function and pokedex text styles

---
+++ lib/Static/pokemon_colors.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class PokemonColors {
+  Color? pokemonCardColor({required String type}) {
+    switch (type) {
+      case 'normal':
+        return Color(0xffb7b8bc);
+      case 'fire':
+        return Color(0xffFFA756);
+      case 'water':
+        return Color(0xff58ABF6);
+      case 'grass':
+        return Color(0xff8BBE8A);
+      case 'electric':
+        return Color(0xffF2CB55);
+      case 'ice':
+        return Color(0xff6ee5d7);
+      case 'fighting':
+        return Color(0xffEB4971);
+      case 'poison':
+        return Color(0xffb85ee5);
+      case 'ground':
+        return Color(0xffF88551);
+      case 'flying':
+        return Color(0xff84a3e0);
+      case 'psychic':
+        return Color(0xffff7275);
+      case 'bug':
+        return Color(0xff8BD674);
+      case 'rock':
+        return Color(0xffd3ba96);
+      case 'ghost':
+        return Color(0xff637cc6);
+      case 'dark':
+        return Color(0xff717075);
+      case 'dragon':
+        return Color(0xff1178d8);
+      case 'steel':
+        return Color(0xff5095b2);
+      case 'fairy':
+        return Color(0xffff84da);
+      default:
+        return Colors.grey;
+    }
+  }
+
+  Color? pokemonTypeColor({required String type}) {
+    switch (type) {
+      case 'normal':
+        return Color(0xffA0A1A5);
+      case 'fire':
+        return Color(0xffFD7E25);
+      case 'water':
+        return Color(0xff4B91DA);
+      case 'grass':
+        return Color(0xff62B958);
+      case 'electric':
+        return Color(0xffEED537);
+      case 'ice':
+        return Color(0xff62CDC1);
+      case 'fighting':
+        return Color(0xffD44D6D);
+      case 'poison':
+        return Color(0xffA552CC);
+      case 'ground':
+        return Color(0xffDD7443);
+      case 'flying':
+        return Color(0xff7590C8);
+      case 'psychic':
+        return Color(0xffEA5D60);
+      case 'bug':
+        return Color(0xff8CB231);
+      case 'rock':
+        return Color(0xffBCA584);
+      case 'ghost':
+        return Color(0xff566CAE);
+      case 'dark':
+        return Color(0xff59585C);
+      case 'dragon':
+        return Color(0xff0F6AC0);
+      case 'steel':
+        return Color(0xff447F9A);
+      case 'fairy':
+        return Color(0xffEE6FC8);
+      default:
+        return Colors.grey;
+    }
+  }
+}
---
+++ lib/components/widgets/home_page_components.dart
@@ -0,0 +1,136 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pokedexv2/Static/pokemon_colors.dart';
+import 'package:pokedexv2/constants/pokemon_constants.dart';
+import 'package:pokedexv2/models/pokemon_page_model.dart';
+import 'package:pokedexv2/Static/text_styles.dart';
+import 'package:pokedexv2/models/pokemon_summary_model.dart';
+import 'package:pokedexv2/util/text_util.dart';
+
+class HomePageComponents {
+  Widget homeSearchBar(BuildContext context, FocusNode focusNode) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 30),
+      color: Colors.transparent,
+      width: MediaQuery.of(context).size.width,
+      height: 50,
+      child: TextField(
+        focusNode: focusNode,
+        textCapitalization: TextCapitalization.sentences,
+        decoration: InputDecoration(
+          floatingLabelBehavior: FloatingLabelBehavior.never,
+          filled: true,
+          fillColor: Color(0xffF4F5F5),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+              borderSide: BorderSide(color: Color(0xffF4F5F5), width: 2)),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            borderSide: BorderSide(color: Color(0xffF4F5F5), width: 1),
+          ),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+              borderSide: BorderSide(color: Color(0xffF4F5F5), width: 1)),
+          labelText: 'What Pokemon are you looking for?',
+          prefixIcon: Icon(
+            Icons.search,
+            color: Colors.black,
+          ),
+          labelStyle: TextStyles().fontStyleRegular(
+            15,
+            Colors.grey.shade800,
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> pokemonTypeButton(List<String>? summaryTypes) {
+    List<Widget> buttons = [];
+
+    for (int i = 0; i < summaryTypes!.length; i++) {
+      buttons.add(
+        Container(
+          margin: EdgeInsets.only(right: i == 0 ? 10 : 0),
+          padding: EdgeInsets.all(5),
+          decoration: BoxDecoration(
+            color: PokemonColors()
+                .pokemonTypeColor(type: summaryTypes[i].toLowerCase())!,
+            borderRadius: BorderRadius.circular(4),
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                PokemonConstants().pokemonTypeUrl +
+                    "${summaryTypes[i].toLowerCase()}.svg",
+                width: 15,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 5),
+                child: Text(
+                  TextUtil().textCapitalize(summaryTypes[i]),
+                  style: TextStyles().fontStyleRegular(12, Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+    return buttons;
+  }
+
+  Widget pokemonCard({context, pokeInfo, index}) {
+    PokemonSummary pokemon = PokemonSummary.fromJson(pokeInfo);
+
+    return Stack(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(left: 30, right: 30, bottom: 10, top: 20),
+          padding: EdgeInsets.all(20),
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: PokemonColors()
+                .pokemonCardColor(type: pokemon.types[0].toLowerCase())!,
+          ),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "#${pokemon.number}",
+                  style: TextStyles().fontStyleBold(12, Colors.black54),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8),
+                  child: Text(
+                    TextUtil().textCapitalize(
+                      pokemon.name.toString(),
+                    ),
+                    strutStyle: StrutStyle(forceStrutHeight: true),
+                    style: TextStyles().fontStyleBold(22, Colors.white),
+                  ),
+                ),
+                Row(
+                  children: pokemonTypeButton(pokemon.types),
+                ),
+              ]),
+        ),
+        Positioned(
+          right: 40,
+          child: CachedNetworkImage(
+            imageUrl: pokemon.imageUrl,
+            width: 125,
+            placeholder: (context, url) => CircularProgressIndicator(),
+            errorWidget: (context, url, error) => Icon(Icons.error),
+          ),
+        ),
+      ],
+    );
+  }
+}
