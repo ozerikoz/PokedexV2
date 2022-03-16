be44b201d714b0dab2ff75d7359050c6b0bcce9d
Erik <erik.paschoalatto1@gmail.com>
2022-03-16 18:30:48 -0300

    feat: route generator implemented

---
+++ lib/routes/route_generator.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:pokedexv2/screens/home_page.dart';
+import 'package:pokedexv2/screens/pokemon_page.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final args = settings.arguments;
+
+    switch (settings.name) {
+      case "/":
+        return MaterialPageRoute(builder: (_) => HomePage());
+
+      case "pokemonPage":
+        if (args is int) {
+          return MaterialPageRoute(
+            builder: (_) => PokemonPage(
+              index: args,
+            ),
+          );
+        }
+        return _errorRoute();
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        body: Text("error"),
+      );
+    });
+  }
+}
