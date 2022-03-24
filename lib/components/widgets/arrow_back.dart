368da4cfb1e128f1af1a738d1a6e2c529b428dd0
Erik <erik.paschoalatto1@gmail.com>
2022-03-24 20:02:45 -0300

    fix: add stream to rotate arrow back icon when open pokemon detail

--- lib/components/widgets/arrow_back.dart
+++ lib/components/widgets/arrow_back.dart
@@ -1,15 +1,49 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
-class ArrowBack extends StatelessWidget {
-  const ArrowBack({Key? key}) : super(key: key);
+StreamController<bool> streamController = StreamController.broadcast();
+
+class ArrowBack extends StatefulWidget {
+  final Stream<bool> stream;
+  const ArrowBack(
+    this.stream, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<ArrowBack> createState() => _ArrowBackState();
+}
+
+class _ArrowBackState extends State<ArrowBack> {
+  int value = 0;
+  @override
+  void initState() {
+    widget.stream.asBroadcastStream().listen((pokemonDetailsOpen) {
+      pokemonDetailsOpen ? value = 3 : value = 0;
+      if (mounted) {
+        setState(() {
+          arrowBack(value);
+        });
+      }
+    });
+    super.initState();
+  }
+
+  Widget arrowBack(int value) {
+    return RotatedBox(
+      quarterTurns: value,
+      child: IconButton(
+        padding: EdgeInsets.only(top: 10),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        icon: Icon(Icons.arrow_back_ios_new_rounded, color: Colors.white),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      onPressed: () {
-        Navigator.pop(context);
-      },
-      icon: Icon(Icons.arrow_back_ios_new_rounded, color: Colors.white),
-    );
+    return arrowBack(value);
   }
 }
